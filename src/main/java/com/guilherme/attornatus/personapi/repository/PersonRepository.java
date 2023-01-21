bafdc21b88be7fdfe8c94c2f6a21eef040cf86f9
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2023-01-21 11:55:40 -0300

    Person Repository

---
+++ src/main/java/com/guilherme/attornatus/personapi/repository/PersonRepository.java
@@ -0,0 +1,6 @@
+package com.guilherme.attornatus.personapi.repository;
+import com.guilherme.attornatus.personapi.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
